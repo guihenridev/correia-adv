065824b8a4fbd14b3dc447601e7fcd780c178357
foskerbr <58860932+foskerbr@users.noreply.github.com>
2022-10-31 23:31:42 -0300

    Estilizando Desktop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 //Menu
 function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
-    if(menuMobile.classList.contains('open')){
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
-    }else{
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
-    }
+    const iconMenu = document.querySelector('.icon');
+    menuMobile.classList.toggle('open');
+    iconMenu.classList.toggle('fa-bars');
+    iconMenu.classList.toggle('fa-xmark');
+
+    // if(menuMobile.classList.contains('open')){
+    //     menuMobile.classList.remove('open');
+        
+    //     iconMenu.classList.add('fa-bars')
+    // }else{
+    //     menuMobile.classList.add('open');
+    //     iconMenu.classList.remove("fa-bars")
+    //     iconMenu.classList.add("fa-xmark");
+    // }
 }
